f931e7f7a2bcfbf195916d5ae1053c0f11dbc552
embeddeath <marquezhdezmiguel@gmail.com>
2024-07-09 08:44:51 -0600

    Added clean option to shell build env

--- shell.py
+++ shell.py
@@ -1,6 +1,8 @@
 
 import argparse
 import subprocess
+import os
+import shutil 
 
 parser = argparse.ArgumentParser(
     prog = 'shell',
@@ -10,9 +12,11 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument(
     'command', 
-    choices = ['build', 'flash'], 
+    choices = ['build', 'flash', 'clean'], 
     help = '1.- [build] = Builds the software.\
-            2.- [flash] = Flash the software to the board.',
+            2.- [flash] = Flash the software to the board.\
+            3.- [clean] = Resets cmake and ninja environment    ',
+            
     )
 
 parser.add_argument(
@@ -36,7 +40,36 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-if (args.command == 'build' and args.debug == True):
+if (args.command == 'clean' and args.debug == True):
+
+    ninja_cwd = './build/Debug'
+    # Clean build folder
+    for filename in os.listdir(ninja_cwd): 
+        file_path = os.path.join(ninja_cwd, filename)
+        try: 
+            if os.path.isfile(file_path) or os.path.islink(file_path): 
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): 
+                shutil.rmtree(file_path)
+        except Exception as e: 
+            print('Failed to delete %s, Reason %s' % (file_path, e))
+
+
+elif (args.command == 'clean' and args.release == True):
+    
+    ninja_cwd = './build/Release'
+    # Clean build folder
+    for filename in os.listdir(ninja_cwd): 
+        file_path = os.path.join(ninja_cwd, filename)
+        try: 
+            if os.path.isfile(file_path) or os.path.islink(file_path): 
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): 
+                shutil.rmtree(file_path)
+        except Exception as e: 
+            print('Failed to delete %s, Reason %s' % (file_path, e))
+
+elif (args.command == 'build' and args.debug == True):
     cmake_command = ['cmake', '--preset', 'Debug']
     ninja_command = ['ninja']
     ninja_cwd = './build/Debug'
@@ -49,6 +82,7 @@ elif (args.command == 'build' and args.release == True):
     ninja_command = ['ninja']
     subprocess.call(cmake_command)
     subprocess.call(ninja_command, cwd = ninja_cwd)
+
 
 elif (args.command == 'flash' and args.release == True): 
     openocd_command = ['openocd', '-f', 'board/st_nucleo_g0.cfg', '-c', ' program template.elf verify reset ', '-c', "shutdown"]
